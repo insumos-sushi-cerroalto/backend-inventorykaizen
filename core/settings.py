741d51bbe804deaa9bccd6590cdc7f321b94b7ec
FelipeNavarro15 <pipe43612@gmail.com>
2026-06-16 21:44:17 -0400

    Eliminar configuración del pooler de Supabase en producción

--- core/settings.py
+++ core/settings.py
@@ -87,12 +87,6 @@ if not DEBUG:
         'options': '-c application_name=yxefkvsfkoamtkmyrbob'
     }
 
-# Configuración obligatoria para el pooler de Supabase en producción
-if not DEBUG:
-    DATABASES['default']['OPTIONS'] = {
-        # Pasamos el ID del proyecto como el application_name para que el proxy sepa a dónde redirigir
-        'options': '-c application_name=yxefkvsfkoamtkmyrbob'
-    }
 
 
 # Password validation
